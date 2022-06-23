c5914c1cb8f6f2864cbc6292d157367a8cbd99df
Borko <bor.kos.mk@gmail.com>
2022-06-23 11:12:50 +0200

    Add files via upload

--- kata.js
+++ kata.js
@@ -27,3 +27,81 @@ function even_or_odd(number) {
     return "Odd";
   }
 }
+
+//Convert a Number to a String!
+
+function numberToString(num) {
+  num = num.toString()
+   return num
+ }
+
+//Even or Odd
+
+function even_or_odd(number) {
+  return number % 2 === 0 ? 'Even' : 'Odd'
+ }
+
+//Opposite number
+
+function opposite(number) {
+  return -number
+}
+
+//Calculate BMI
+
+function bmi(weight, height) {
+  product = weight/(height*height);
+  if(product <= 18.5){
+    return "Underweight"
+  } else if(product <= 25.0) {
+    return "Normal"
+  }else if(product <= 30.0) {
+    return "Overweight"
+  }else if(product => 300) {
+    return "Obese"
+  }else{
+    return ""
+  }
+}
+
+bmi(154, 5.5)
+
+//Returning Strings
+
+function greet(name){
+  return `Hello, ${name} how are you doing today?`;
+}
+
+//Multiply
+
+function multiply(a, b){
+  return a * b
+}
+
+//In this simple assignment you are given a number and have to make it negative. 
+//But maybe the number is already negative?
+
+function makeNegative(num) {
+  return -Math.abs(num)
+}
+makeNegative(1)
+
+
+// You get an array of numbers, return the sum of all of the positives ones.
+// Example [1,-4,7,12] => 1 + 7 + 12 = 20
+// Note: if there is nothing to sum, the sum is default to 0.
+
+function positiveSum(arr) {
+  let sum = 0;
+  for(let i = 0; i < arr.length; i++) {
+      if(arr[i] > 0) sum += arr[i]
+  }
+  return sum
+}
+
+
+function positiveSum(arr) {
+  let sum = 0
+  arr.forEach(num => num > 0 && (sum += num))
+  return sum
+}
